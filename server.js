9e3cc4a1d67d277ae53676fb79d239a17cee0b55
ArmelAtayi <aatayi@Charlotte.edu>
2024-09-14 16:45:49 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 // Budget API
 
+// Budget API
+
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+let budget;
+
+// Read budget data from the JSON file
+fs.readFile('budget-data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Error reading budget data:", err);
+        return;
+    }
+    budget = JSON.parse(data);
+});
 
 app.use('/', express.static('public'))
 
